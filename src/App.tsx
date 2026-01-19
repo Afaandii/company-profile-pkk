8937f7bb0cc56d070ba1687c4388b5a29adcf2c1
Afaandii <fandisantuy140@gmail.com>
2026-01-19 21:34:43 +0700

    improve tampilan

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import Hero from "./components/sections/Hero";
-import About from "./components/sections/About";
-import Templates from "./components/sections/Templates";
-import Pricing from "./components/sections/Pricing";
-import Contact from "./components/sections/Contact";
-import Testimonials from "./components/sections/Testimonials";
+import LandingPage from "./pages/LandingPage";
+import AllTemplates from "./pages/AllTemplates";
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <Hero />
-      <About />
-      <Templates />
-      <Pricing />
-      <Testimonials />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/templates" element={<AllTemplates />} />
+      </Routes>
       <Footer />
     </div>
   );
